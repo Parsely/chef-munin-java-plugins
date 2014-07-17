f7b2859c70fc02c7903e9c00f926960cac05f9c9
Cody Wilbourn <cody@parsely.com>
2014-07-17 08:37:27 -0500

    Automatic configuration of JMX port
    Will install but not configure plugins if no JMX is running.
    Bumped cookbook version

--- README.md
+++ README.md
@@ -26,6 +26,27 @@ Just include `munin-java-plugins` in your node's `run_list`:
 }
 ```
 
+The munin java plugins leverage JMX to communicate to the JVM about the state of 
+your application.
+
+The recipe will automatically detect any running java processes in your ps tree and
+find the JMX port it is using. It will specifically look for `jmxremote.port=(\d+)`,
+
+Example JVM argument: `-Dcom.sun.management.jmxremote.port=7199` 
+
+If it finds the port, which in this case is 7199, the recipe will create a plugin
+configuration file (generally located in /etc/munin/plugin-conf.d). Munin reads
+these files alphabetically and takes the last configuration block. Thus
+munin-node-jmx will be read after munin-node (package installed file), and if 
+you need any further configuration, create a file in this directory to be read 
+after munin-node-jmx.
+
+If no port is found, the munin java plugins will be installed but not setup to run
+(i.e. no symlink from /etc/munin/plugins to the plugin)
+
+*Note*: This recipe is written for only 1 JMX per host. Further improvement is 
+needed to support more than one per host.
+
 Contributing
 ------------
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,27 +10,49 @@
 include_recipe "munin::client"
 
 # install munin-java plugins
-%w{munin munin-java-plugins}.each do |pkg|
-    package pkg
+package 'munin'
+if node['platform'] == "ubuntu" && node['platform_version'] >= "14.04"
+  package 'munin-plugins-java'
+else
+  package 'munin-java-plugins'
 end
 
-bash 'configure Munin JMX plugin' do
-  code <<-EOH
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ClassesLoaded 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ClassesLoadedTotal 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_CurrentThreadCpuTime 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_CurrentThreadUserTime 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_GCCount 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_GCTime 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_MemoryAllocatedHeap 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_MemoryAllocatedNonHeap 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_emorySurvivorPeak 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_MemorySurvivorUsage 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_Threads 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ThreadsDaemon 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ThreadsDeadlocked 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ThreadsPeak 
-sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ThreadsStartedTotal 
+# Capture all Java processes running JMX, making sure we exclude the grep
+java_processes =  `ps aux | grep [j]mxremote`
+regex_matches = /jmxremote\.port=(\d+)/.match(java_processes) if java_processes
+jmx_port = regex_matches[1] if regex_matches
+
+if jmx_port
+  # Create file in /etc/munin/plugin-conf.d/ to be read after munin-node (alphabetically read)
+  template "/etc/munin/plugin-conf.d/munin-node-jmx" do
+    source 'munin-node-jmx.erb'
+    mode   '0644'
+    variables({
+      :port => jmx_port
+    })
+    notifies :restart, resources(:service => "munin-node"), :delayed
+  end
+
+  # Create munin plugin links
+  bash 'configure Munin JMX plugin' do
+    code <<-EOH
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ClassesLoaded
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ClassesLoadedTotal
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_CurrentThreadCpuTime
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_CurrentThreadUserTime
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_GCCount
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_GCTime
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_MemoryAllocatedHeap
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_MemoryAllocatedNonHeap
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_emorySurvivorPeak
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_MemorySurvivorUsage
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_Threads
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ThreadsDaemon
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ThreadsDeadlocked
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ThreadsPeak
+sudo ln -s /usr/share/munin/plugins/jmx_ /etc/munin/plugins/jmx_ThreadsStartedTotal
 EOH
-  not_if { ::File.exists?("/etc/munin/plugins/jmx_ClassesLoaded") }
+    not_if { ::File.exists?("/etc/munin/plugins/jmx_ClassesLoaded") }
+    notifies :restart, resources(:service => "munin-node"), :delayed
+  end
 end
